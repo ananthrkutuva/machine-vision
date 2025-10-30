ce66eaac2e43f72e58f1399f9bb2b435bd5689d9
Ananth Kutuva <ananthkutuva@gmail.com>
2025-10-30 16:38:43 -0400

    made mask for red gloves, still seeing blue things

--- glove_detection.py
+++ glove_detection.py
@@ -9,19 +9,19 @@ path = "/home/akutuva/ros2_ws/src/machine-vision/example_handlebars.jpg"
 img = cv2.imread(path, cv2.IMREAD_COLOR)
 img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# Create HSV bounds for color of gloves
+# Create HSV bounds for color of gloves and color of sky
 red_lower = np.array([0, 145, 145])
 red_upper = np.array([179, 255, 255])
 
-# Create a mask for the red range
-red_mask = cv2.inRange(img_HSV, red_lower, red_upper)
+blue_lower = np.array([223, 40, 70])
+blue_upper = np.array([225, 255, 255])
 
-# Applying mask to image
-result = cv2.bitwise_and(img, img, mask=red_mask)
+result = cv2.inRange(img_HSV, red_lower, red_upper)
 
 # Display the original image, the mask, and the result
 cv2.imshow('Original Image', img)
-cv2.imshow('Red Mask', red_mask)
+cv2.imshow('HSV Image', img_HSV)
+# cv2.imshow('Red Mask', red_mask)
 cv2.imshow('Red Detected', result)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
