08f429b489969dfaa5b02993d137038c5c33a31f
clinehan <lineha.cian28@gmail.com>
2025-11-03 16:30:38 -0500

    Working on handlebars

--- handlebar_detection.py
+++ handlebar_detection.py
@@ -1,20 +1,69 @@
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+import sys
 
-# Image path
-path = "/home/akutuva/ros2_ws/src/machine-vision/example_handlebars.jpg"
+# Video path
+import os
+path = "data/SamplePOV.mp4"
+print(os.path.exists(path))
 
-# Input image into openCV
-img = cv2.imread(path, cv2.IMREAD_COLOR)
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# Show the image
-cv2.imshow('Original Image', img)
-cv2.imshow('RGB Image', img_rgb)
+if __name__ == '__main__' :
 
-# Wait until any key is pressed
-cv2.waitKey(0)
+    tracker_type = 'CSRT'
+    #BOOSTING, MIL, KCF, TLD, MEDIANFLOW, GOTURN, MOOSE
+    tracker = cv2.legacy.TrackerCSRT_create()
 
-# Once a key is pressed, close the window
-cv2.destroyAllWindows()
+    #Read Video
+    vid = cv2.VideoCapture(path)
+
+    # Exit if video not opened.
+    if not vid.isOpened():
+        print("Could not open video")
+        sys.exit()
+ 
+    # Read first frame.
+    ok, frame = vid.read()
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+
+    #Create Bounding Box
+    bb_x = 90
+    bb_y = 23
+    bb_w = 320
+    bb_h = 80
+
+    bbox = (bb_x, bb_y, bb_w, bb_h)
+
+    while True:
+        #Get new frame
+        ok, frame = vid.read()
+        if not ok:
+            break
+
+        #Timer
+        timer = cv2.getTickCount()
+
+        #Update Tracker
+        ok, bbox = tracker.update(frame)
+
+        #fps
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+
+        #Draw Bounding Box
+        if ok:
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+        else:
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+
+
+        cv2.imshow("Tracking", frame)
+
+
+        k = cv2.waitKey(1) & 0xff
+        if k == 27 : break
+
