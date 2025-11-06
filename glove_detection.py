ef49a35a1641d9bafe3e5a1f8acba478dd6df093
Ananth Kutuva <ananthkutuva@gmail.com>
2025-11-06 15:22:37 -0500

    made contours around red gloves show up

--- glove_detection.py
+++ glove_detection.py
@@ -6,27 +6,24 @@ import numpy as np
 path = "/home/akutuva/ros2_ws/src/machine-vision/example_handlebars.jpg"
 
 # Input image into openCV
-img = cv2.imread(path, cv2.IMREAD_COLOR)
-img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+original_img = cv2.imread(path, cv2.IMREAD_COLOR)
+img_HSV = cv2.cvtColor(original_img, cv2.COLOR_BGR2HSV)
 
 # Create HSV bounds for color of gloves and color of sky
-red_lower = np.array([150, 145, 145])
+red_lower = np.array([110, 145, 145])
 red_upper = np.array([179, 255, 255])
 
 red_mask = cv2.inRange(img_HSV, red_lower, red_upper)
 
+contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-_, binary_image = cv2.threshold(red_mask, 127, 255, cv2.THRESH_BINARY)
-
-contours, hierarchy = 
-
-
-mask_applied = cv2.bitwise_and(img, img, mask=red_mask)
+image_with_contours = cv2.drawContours(original_img, contours, -1, (0, 255, 0), 3)
 
 # Display the original image, the mask, and the result
-cv2.imshow('Original Image', img)
+cv2.imshow('Original Image', original_img)
 cv2.imshow('HSV Image', img_HSV)
 cv2.imshow('Red Detected', red_mask)
-cv2.imshow('Final Image', mask_applied)
+cv2.imshow('Original Image with Contours Overlayed', image_with_contours)
+# cv2.imshow('Final Image', mask_applied)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
